f94a2ea0eb8c55b43828dbc6a878a51a477ec1a3
Mika Raento <mikie@iki.fi>
2012-10-11 14:38:19 +0300

    address review comments

--- example.i
+++ example.i
@@ -3,5 +3,5 @@ int bar(int x);
 
 %{
 static int foo() { return 3; }
-static int bar(int x) { return 3 * x; }
+static int bar(int x) { return 30 * x; }
 %}
--- mkswigfile.clj
+++ mkswigfile.clj
@@ -4,13 +4,13 @@
 
 (ns mkswigfile
   "Loading of new swig code into your jvm - like mkoctfile for octave.
-  
+
    The swig code must:
       - only declare functions, not classes
       - use not-publicly-visible implementations (either
         use static or an anonumous namespace).
       - not contain a %module statement
-  
+
    You should be able to use whatever typemaps you think
    make sense for your code."
   (:use clojure.java.io
@@ -19,7 +19,7 @@
 (defn- make-def
   "Define a function in this namespace that has the same name
    as the given (static) Method and calls the method.
-  
+
    We have to use intern and not defn (even through a macro)
    as all macros are expanded at compile time."
   [^java.lang.reflect.Method method]
@@ -38,22 +38,22 @@
                     (apply str (concat ["Failed to execute command "]
                                         (interleave args (repeat " "))
                                         [" returned " (str ret)])))))))
-                                        
+
 (defn mkswigfile
   "Compile the given swig file that should only have function declarations
    (no classes), load the new code (class) and define the functions in
    the swig file in this namespace.
-  
+
    The default flags only work on OS-X jni.h in the place where my Java
    installation put it, and require code to pass as c++."
   [^String filename0
-   &{ :keys [swig cc swigflags ccflags]
-      :or {swig "swig"
-           cc "g++"
-           ccflags ["-fPIC" "-shared"
-                    "-flat_namespace"
-                    "-I/System/Library/Frameworks/JavaVM.framework/Headers/"]
-           swigflags ["-c++"]}}]
+   &{:keys [swig cc swigflags ccflags]
+     :or {swig "swig"
+          cc "g++"
+          ccflags ["-fPIC" "-shared"
+                   "-flat_namespace"
+                   "-I/System/Library/Frameworks/JavaVM.framework/Headers/"]
+          swigflags ["-c++"]}}]
   (let [^String contents (slurp (as-file filename0))
         filename (.replaceAll filename0 "/" "_")
         ;; Use java tmpdir for the created swig, java and c++ source
@@ -61,13 +61,13 @@
         tmpdir (as-file (System/getProperty "java.io.tmpdir"))
         id (str "mksf" (.replaceAll (.toString (java.util.UUID/randomUUID))
                                     "-" ""))
-        dir (file tmpdir (as-file id))
+        out-dir (file tmpdir (as-file id))
         wrapped (.replace filename ".i" "_wrap.cxx")
-        i-file (file dir (as-file filename))
-        jnilib-file (file dir (as-file "libmksf.so"))
+        i-file (file out-dir (as-file filename))
+        jnilib-file (file out-dir (as-file "libmksf.so"))
         jnilib (.getAbsolutePath jnilib-file)]
-    (assert (not (.exists dir)))
-    (assert (.mkdir dir))
+    (assert (not (.exists out-dir)))
+    (assert (.mkdir out-dir))
     (with-open [i-writer (writer i-file)]
       ;; The module becomes the java class name, have to have one
       (.write i-writer (str "%module mkswigfile\n"))
@@ -77,7 +77,7 @@
       (.write i-writer "%pragma(java) jniclasscode=%{\n")
       (.write i-writer (str "  static { System.load(\"" jnilib "\"); }"))
       (.write i-writer "%}\n"))
-    (with-sh-dir dir
+    (with-sh-dir out-dir
       (apply sh-or-die (concat [swig]
                                swigflags
                                ["-package" id "-java" filename]))
